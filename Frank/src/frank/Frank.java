176bb6749080462feacd209914b13c80ad24c232
Shane Pudner <setherith@gmail.com>
2018-07-18 17:18:01 +0100

    Project stub with some initial code

---
+++ Frank/src/frank/Frank.java
@@ -0,0 +1,19 @@
+package frank;
+
+/**
+ *
+ * @author Shane Pudner <setherith@gmail.com>
+ */
+public class Frank {
+
+    public static void main(String[] args) {
+        Frank frank = new Frank();
+        System.out.print("Quiting...");
+    }
+    
+    public Frank()
+    {
+        
+    }
+    
+}
---
+++ Frank/src/utilities/Randomness.java
@@ -0,0 +1,19 @@
+package utilities;
+
+/**
+ *
+ * @author Shane Pudner <setherith@gmail.com>
+ */
+public class Randomness {
+    
+    /**
+     * @param <T> Any enumerable object
+     * @param array An array object
+     * @return Object of type within passed in array
+     */
+    public static <T> T GetRandomElement(T[] array) {
+        int length = array.length;
+        int element = (int) (Math.random() * length);
+        return array[element];
+    }
+}
