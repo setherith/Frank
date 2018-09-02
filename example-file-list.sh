d44eed24e247cdc23ae82e13cde1706d46ce5228
Shane <setherith@hotmail.co.uk>
2018-09-02 21:58:31 +0100

    Added file to create test cases for me

---
+++ example-file-list.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo "Test script for creating files for Frank"
+echo "Written by Shane Pudner (2018)"
+
+echo "Creating folder..."
+
+mkdir test_folder
+cd test_folder/
+
+touch supernatural.s07e01.720p.hdtv.x264-orenji.srt
+touch supernatural.s07e02.720p.hdtv.x264-2hd.srt
+touch supernatural.S07E03.720p.HDTV.x264-IMMERSE.srt
+touch supernatural.s07e04.720p.hdtv.x264-orenji.srt
+touch supernatural.s07e05.720p.hdtv.x264-compulsion.srt
+touch supernatural.S07E06.720p.HDTV.x264-IMMERSE.srt
+touch supernatural.S07E07.720p.HDTV.x264-IMMERSE.mkv
+touch supernatural.S07E07.720p.HDTV.x264-IMMERSE.srt
+touch supernatural.S07E08.720p.HDTV.x264-IMMERSE.mkv
+touch supernatural.S07E08.720p.HDTV.x264-IMMERSE.srt
+touch supernatural.s07e09.proper.720p.hdtv.x264-2hd.mkv
+touch supernatural.s07e09.proper.720p.hdtv.x264-2hd.srt
+touch supernatural.S07E10.720p.HDTV.x264-IMMERSE.mkv
+touch supernatural.S07E10.720p.HDTV.x264-IMMERSE.srt
+touch supernatural.S07E11.720p.HDTV.x264-IMMERSE.mkv
+touch supernatural.S07E11.720p.HDTV.x264-IMMERSE.srt
+touch supernatural.s07e12.720p.hdtv.x264-2hd.mkv
+touch supernatural.s07e12.720p.hdtv.x264-2hd.srt
+touch supernatural.S07E13.720p.HDTV.X264-DIMENSION.mkv
+touch supernatural.S07E13.720p.HDTV.X264-DIMENSION.srt
+touch supernatural.S07E14.720p.HDTV.X264-DIMENSION.mkv
+touch supernatural.S07E14.720p.HDTV.X264-DIMENSION.srt
+touch "supernatural.S07E15.720p.HDTV.X264-DIMENSION(480p).mkv"
+touch "supernatural.S07E15.720p.HDTV.X264-DIMENSION(480p).srt"
+touch supernatural.S07E16.480p.HDTV.X264-MSTM.mkv
+touch supernatural.S07E16.480p.HDTV.X264-MSTM.srt
+touch supernatural.S07E17.480p.HDTV.X264-DIMENSION.mkv
+touch supernatural.S07E17.480p.HDTV.X264-DIMENSION.srt
+touch supernatural.S07E18.480p.DLBR.mkv
+touch supernatural.S07E18.480p.DLBR.srt
+touch supernatural.S07E19.720p.HDTV.X264-DIMENSION.mkv
+touch supernatural.S07E19.720p.HDTV.X264-DIMENSION.srt
+touch supernatural.S07E20.720p.HDTV.X264-DIMENSION.mkv
+touch supernatural.S07E20.720p.HDTV.X264-DIMENSION.srt
+touch "supernatural.S07E21.720p.HDTV.X264-DIMENSION(480p).mkv"
+touch "supernatural.S07E21.720p.HDTV.X264-DIMENSION(480p).srt"
+touch supernatural.S07E22.720p.HDTV.X264-DIMENSION.mkv
+touch supernatural.S07E22.720p.HDTV.X264-DIMENSION.srt
+touch "supernatural.S07E23.720p.HDTV.X264-DIMENSION(480p).mkv"
+touch "supernatural.S07E23.720p.HDTV.X264-DIMENSION(480p).srt"
+cd ..
+
+echo "Finish creating test files"
+echo "Enjoy!"
