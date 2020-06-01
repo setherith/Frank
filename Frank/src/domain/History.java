5537a368f3e99ea0428514243876fedb1a1d334b
Shane Pudner <shane.pudner@babcockinternational.com>
2020-06-01 09:59:30 +0100

    Added History model for renaming later on...

---
+++ Frank/src/domain/History.java
@@ -0,0 +1,31 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class History {
+
+	private List<String> Names;
+	
+	public History(String originalName) {
+		Names = new ArrayList<String>();
+		Names.add(originalName);
+	}
+	
+	public String getOriginalName() {
+		return Names.get(0);
+	}
+	
+	public String getLatestName() {
+		if (Names.size() > 0) {
+			return Names.get(Names.size() - 1);
+		} else {
+			return getOriginalName();
+		}
+	}
+	
+	public void updateName(String name) {
+		Names.add(name);
+	}
+	
+}
